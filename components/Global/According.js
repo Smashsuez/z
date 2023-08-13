f40e3ab3b48f19c041c8b7c2e372265b38f899ba
Smashsuez <mohamedahmedgameel@gmail.com>
2023-08-14 00:12:06 +0300

    .

--- components/Global/According.js
+++ components/Global/According.js
@@ -34,7 +34,7 @@ const According = () => {
                         }
                     </div>
                     <div className='right'>
-                        <img src='/imgs/earth (2).jpg' alt='Tower'/>
+                        <img src='/imgs/according.jpg' alt='Tower'/>
                     </div>
                 </div>
             </div>
