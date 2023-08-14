f2b9a0ebbb61ac9bc9899c1f925a10951d6fa754
Smashsuez <mohamedahmedgameel@gmail.com>
2023-08-14 15:55:08 +0300

    .

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Global/Header'
 import Footer from '@/components/Global/Footer'
-import { AnalyticsProps } from '@vercel/analytics/*'
 
 const inter = Inter({ subsets: ['latin'] })
 
