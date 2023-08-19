9140037786eca52e34ac33fea2299e8f0abdefac
Smashsuez <mohamedahmedgameel@gmail.com>
2023-08-19 18:42:11 +0300

    ,

--- components/Auth/SignIn.js
+++ components/Auth/SignIn.js
@@ -17,7 +17,7 @@ const SignIn = ({ callbackUrl }) => {
       setMessage(res.msg)
     }
     catch (error) {
-      setMessage('Please Check Your Personal Data');
+      setMessage('Data does not exist!');
     }
     
   }
