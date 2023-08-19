96759a2cc8702b5bcdfc856e4d61951d1af1c3b5
Smashsuez <mohamedahmedgameel@gmail.com>
2023-08-19 19:59:58 +0300

    .

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -92,7 +92,7 @@ async function getUserByEmail({email}){
 
 async function signInWithCredentials({email, password, phone}){
     const user = await User.findOne({email})
-    if(!user) throw new Error("Email Does Not Exist")
+    if(!user) return{msg: "email does not exist!"}
 
     const compare = await bcrypt.compare(password, user.password)
     if (!compare) return{msg: "Password Does not match"}
