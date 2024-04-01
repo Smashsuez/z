f2c7f3ff3b4504f4c44dff05da172d8ea34d68b8
Smashsuez <mohamedahmedgameel@gmail.com>
2024-04-01 18:50:52 +0200

    adsense

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,47 @@
-import Provider from '@/context/provider'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Header from '@/components/Global/Header'
-import Footer from '@/components/Global/Footer'
-import { Analytics } from '@vercel/analytics/react';
+import Provider from "@/context/provider";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "@/components/Global/Header";
+import Footer from "@/components/Global/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
+// export const metadata = {
+//   description: 'MyZagel Website',
+// }
 export const metadata = {
-  description: 'MyZagel Website',
-}
+  name: "google-adsense-account",
+  content: "ca-pub-8234874125207307",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head>
+      <head>
         <title>ZAGEL</title>
         <meta name="description" content="MyZagel Website" />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
+        <link
+          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+          rel="stylesheet"
+        ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="google-site-verification" content="83LjU3ij1FIvWD7zMwXOt7CHlmUN-6cJuoHgcO0wGV8" />
-        
+        <meta
+          name="google-site-verification"
+          content="83LjU3ij1FIvWD7zMwXOt7CHlmUN-6cJuoHgcO0wGV8"
+        />
       </head>
       <body className={inter.className}>
         <Provider>
-          <Header/>
-          <main >
+          <Header />
+          <main>
             {children}
             <Analytics />
           </main>
-          <Footer/>
+          <Footer />
         </Provider>
       </body>
-      
     </html>
-  )
+  );
 }
