2decbe7186ea7caa2ca02f6a3600307179408b94
Smashsuez <mohamedahmedgameel@gmail.com>
2024-04-01 18:54:17 +0200

    adsense script

--- context/provider.js
+++ context/provider.js
@@ -1,13 +1,13 @@
-'use client'
-import { SessionProvider } from 'next-auth/react'
+"use client";
+import { SessionProvider } from "next-auth/react";
+<script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8234874125207307"
+  crossorigin="anonymous"
+></script>;
 
-
-const Provider = ({children}) => {
-    return(
-        <SessionProvider>
-            { children }
-        </SessionProvider>
-    )
-}
+const Provider = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
 
 export default Provider;
