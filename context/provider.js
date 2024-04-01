98da1733ceb464817e9894365b22645b2e9793cc
Smashsuez <mohamedahmedgameel@gmail.com>
2024-04-01 18:56:39 +0200

    .

--- context/provider.js
+++ context/provider.js
@@ -1,10 +1,6 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-<script
-  async
-  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8234874125207307"
-  crossorigin="anonymous"
-></script>;
+
 
 const Provider = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
